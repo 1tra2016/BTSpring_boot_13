16abfa927b4ac2f107d78bb57b66aafe1cd59ce3
Chè <22010087@st.phenikaa-uni.edu.vn>
2026-03-29 15:04:17 +0700

    kha1ss13 add secury '

--- src/main/java/springboot/ss11/controller/SuppliesController.java
+++ src/main/java/springboot/ss11/controller/SuppliesController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping({"/api/v1/supplies"})
+@RequestMapping({"/api/v1/employees"})
 @RequiredArgsConstructor
 public class SuppliesController {
     private final ISuppliesService supService;

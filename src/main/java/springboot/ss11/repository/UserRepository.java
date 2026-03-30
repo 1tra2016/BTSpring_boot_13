121984e98633e2fdfb557acfff0a2ddcc52f8b42
Chè <22010087@st.phenikaa-uni.edu.vn>
2026-03-30 15:07:23 +0700

    gioi2ss13

--- src/main/java/springboot/ss11/repository/UserRepository.java
+++ src/main/java/springboot/ss11/repository/UserRepository.java
@@ -6,5 +6,5 @@ import springboot.ss11.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/springboot/ss11/security/UserDetailServiceCustom.java
@@ -0,0 +1,30 @@
+package springboot.ss11.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import springboot.ss11.entity.User;
+import springboot.ss11.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceCustom implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new UserPrincipal(user);
+    }
+}
